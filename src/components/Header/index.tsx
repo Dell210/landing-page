778143ed51acc8474ad34db9fc234f1e9d829881
Dell210 <arthur18colliramos@gmail.com>
2024-06-11 18:59:32 -0300

    add: hover in button-default and component Tickets

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,11 +7,13 @@ export function Header() {
 
         <nav>
             <ul>
-                <li> <a href=''>Sobre</a> </li>
-                <li> <a href=''>Cronograma</a> </li>
-                <li> <a href=''>Ingressos</a> </li>
-                <li> <a href=''>Localização</a> </li>
-                <li> <a href=''>Contato</a> </li>
+                <li> <a href="#section-hero">Home</a> </li>
+                <li> <a href="">Sobre</a> </li>
+                <li> <a href="">Cronograma</a> </li>
+                <li> <a href="#section-tickets">Ingressos</a> </li>
+                <li> <a href="">Localização</a> </li>
+                <li> <a href="#section-form">Newsletter</a> </li>
+                <li> <a href="">Contato</a> </li>
             </ul>
         </nav>
     </header>
